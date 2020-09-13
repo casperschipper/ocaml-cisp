8cede1df72e5e2d89d279c19b48f466dc7835873
Caspers QuadMBP <casper.schipper@gmail.com>
2020-09-13 17:03:28 +0200

    OSC output, I still have to test timing...

--- examples/cisp1.ml
+++ examples/cisp1.ml
@@ -1,23 +1,10 @@
 open Cisp
-open Midi
 open Seq
 
-let sr = ref 44100.0
 
-let channel = 1 
+let timer = st 1.5
 
-(* this maps midi input msg to an output msg (raw midi) *)
-let midiInputTestFun input =
-  (* this translates input into boolean trigger *)
-  (* a metre boolean mask that triggers notes or rests in the pattern *)
-  (* dummy event *)
-  (* apply metre to default event, use Silence as filler *)
-  input
-  |> trigger (mkRhythm (st c3) (seq [1;1;3;5]) (seq [2;3])) 
-  |> withPitch (seq [60;64;76])
-  |> withDur (ch [|425|])
-  |> withChan (st 1)
-  |> withVelo (st 100)
-  |> serialize |> map toRaw
-                         
-let () = Midi.playMidi midiInputTestFun sr 
+let values = seq [11;12;13] |> map Int.to_float
+
+let () =
+    Lwt_main.run ( OscStream.play "127.0.0.1" 56789 "/par1" timer values )
---
+++ src/oscStream/oscStream.mli
@@ -0,0 +1,3 @@
+val play : string -> int -> string -> float Seq.t -> float Seq.t -> unit Lwt.t
+
+

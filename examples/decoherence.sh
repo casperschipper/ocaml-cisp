4178b8bc827e0a69f20386e47ed78299cfd08020
Caspers QuadMBP <casper.schipper@gmail.com>
2020-10-28 09:26:38 +0100

    Let's hope it works

--- examples/decoherence.sh
+++ examples/decoherence.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dune exec ./decoherence.exe &
-sleep 3
+sleep 5
 echo "ok start connecting"
-jack_lsp
+
 jack_connect system:capture_1 ocaml:input_0
 jack_connect system:capture_1 ocaml:input_1
 jack_connect system:capture_1 ocaml:input_2
@@ -11,3 +11,7 @@ jack_connect system:capture_1 ocaml:input_4
 jack_connect system:capture_1 ocaml:input_5
 jack_connect system:capture_1 ocaml:input_6
 jack_connect system:capture_1 ocaml:input_7
+
+sleep 1
+
+jack_lsp

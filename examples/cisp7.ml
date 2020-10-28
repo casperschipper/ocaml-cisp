b49151d5ed903d86eddb33bf7cc11796b7a11335
Casper Schipper <casper.schipper@gmail.com>
2020-10-28 14:20:48 +0100

    before thing is integrated

--- examples/cisp7.ml
+++ examples/cisp7.ml
@@ -12,15 +12,16 @@ let sumEight =
 (* TODO REMOVE MASTERCLOCK IF COPIED INTO MAIN *)
                   
 let () =
-  let wl = rvf (st (-80.0)) (st 0.0) |> map mtof |> map (fun x -> 1.0 /. x) in
-  let bottom = tmd (lift rvf 0.0 3.0) (seq [0.0;sec 10.0]) in
-  let top = tmd (lift rvf 0.0 3.0) (seq [0.0;sec 10.0]) in
+  let wl = rvf (st (-64.0)) (st 30.0) |> map mtof |> map (fun x -> 1.0 /. x) in
+  let bottom = tmd (lift rvf 1.0 7.0) (seq [0.0;sec 10.0]) in
+  let top = tmd (lift rvf 1.0 9.0) (seq [0.0;sec 10.0]) in
   let buffer = Array.make (sec 10.0 |> Int.of_float) 0.0 in
-  let writer = write buffer (countTill <| cap buffer) (sumEight |> map tanh |> ( ( *.~ ) (st 0.8) ) |> bhpf_static 50.0 0.9)  in
+  let writer = write buffer (countTill <| cap buffer) (sumEight  |> map tanh |> ( ( *.~ ) (st 0.8) )) in
   let myIndex = tline (tmd (rvf (st 0.5) (st 5.0)) wl) ([bottom;top] |> ofList |> transpose |> concat) in
-  let mkOut () = indexLin buffer myIndex in
-  let joined = effect writer (mkOut ()) in
-  Jack.playSeqs 8 Process.sample_rate [ effect masterClock joined ; mkOut ()] 
+  let mkOut = indexLin buffer myIndex in
+  let joined = effect writer mkOut in
+
+  Jack.playSeqs 8 Process.sample_rate [ effect masterClock joined; mkOut] 
 
      
 
--- examples/cisp8.ml
+++ examples/cisp8.ml
@@ -3,6 +3,7 @@ open Cisp
 (* open Format   *)
 
 let () =
-  Jack.playSeqs 1 Process.sample_rate [effect masterClock (Process.inputSeq 0 +.~ (st 1.0) |> leakDC 0.995) ]
+  let testSig = (st 1.0) +.~ (osc (st  220.0)) in
+  Jack.playSeqs 1 Process.sample_rate [effect masterClock (Process.inputSeq 0 +.~ testSig |> leakDC 0.5) ]
                                          
 

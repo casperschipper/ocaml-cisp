4a0c477d4525c754eb385fc12f94c6f2af80c34a
Caspers QuadMBP <casper.schipper@gmail.com>
2021-10-08 13:12:00 +0200

    Ivo.ml: trying to hook up both midi and osc in one file.

--- examples/ivo.ml
+++ examples/ivo.ml
@@ -32,27 +32,6 @@ let fmap f (OscParser p) =
   let mapFst f (a, s) = (f a, s) in
   OscParser (fun s -> p s |> Result.map (mapFst f))
 
-(*
-    
-let join_result res_sq =
-  let module R = Result in
-  let rec aux in_sq acc =
-    match acc with
-    | Error err -> Error err
-    | Ok accu ->
-      begin
-        match in_sq () with
-        | Seq.Nil -> Ok accu
-        | Seq.Cons(R.Ok sq,tail) ->
-          let newAccu = R.Ok (Seq.append sq accu)in
-          aux tail newAccu
-        | Seq.Cons(R.Error err,_) ->
-          R.Error err
-      end
-  in
-  aux res_sq (R.Ok Seq.empty)
-*)
-
 let bind p f =
   let step result =
     match result with
@@ -108,11 +87,11 @@ let int =
   >>= fun d ->
   match d with `Int32 i -> return i | _ -> failure "expected an int"
 
-let floatParameter parName =
+let flt_par parName =
   let err = "Expected parameter name: " ^ parName in
   satisfy (isString parName) err >> float
 
-let intParameter parName =
+let int_par parName =
   let err = "Expected int parameter withname: " ^ parName in
   satisfy (isString parName) err >> int
 
@@ -120,11 +99,11 @@ let message =
   let mk voice_nr start_t end_t =
     Set {voiceNr= voice_nr; preset= Preset {start_t; end_t}}
   in
-  intParameter "voiceNr"
+  int_par "voiceNr"
   >>= fun n ->
-  floatParameter "start"
+  flt_par "start"
   >>= fun start_t ->
-  floatParameter "end"
+  flt_par "end"
   >>= fun end_t ->
   print_float end_t ;
   print_string "endt" ;
@@ -132,6 +111,7 @@ let message =
   let made = mk n start_t end_t in
   return made
 
+(* OOF this is nasty, always include a unit in the function definition, otherwise it is never evaluated again *)
 let update (Set {voiceNr; preset}) () =
   print_endline "write!" ;
   myState.(voiceNr) <- Some preset
@@ -195,9 +175,193 @@ let myHandler path d =
       print_string ("unknown path: " ^ path ^ "\n") ;
       flush stdout
 
-let () =
+let bang = ref false
+
+let midiLoop input =
+  input
+  |> Seq.map (fun i ->
+         i |> Midi.isNoteOn |> ( := ) bang ;
+         Midi.MidiSilence |> Midi.toRaw )
+
+let midi_in () = Midi.playMidi midiLoop Process.sample_rate
+
+let osc_receive () =
   let port = 7777 in
   let s = S.create port myHandler in
   while true do
     S.recv s
+  done
+
+module UniqueId = struct
+  type t = Pool of {free: int list; used: int list}
+
+  type id = Id of int
+
+  let maxId = 65536
+
+  let init = Pool {free= List.init maxId (fun x -> x + 1); used= []}
+
+  let free (Pool pool) id =
+    Pool {used= List.filter (fun x -> x = id) pool.used; free= id :: pool.free}
+
+  let get (Pool pool) =
+    match pool.free with
+    | id :: ids ->
+        let newPool = Pool {free= ids; used= id :: pool.used} in
+        (Id id, newPool)
+    | [] -> (Id 1, Pool {free= List.init maxId (fun x -> x + 2); used= [1]})
+
+  let isFree (Pool pool) (Id id) = List.mem id pool.free
+end
+
+module PresetMap = Map.Make (Int)
+
+module PresetMemory = struct
+  type preset =
+    | Preset of
+        { start: float
+        ; slicedur: float
+        ; rate: float
+        ; bufnum: int
+        ; amp: float
+        ; pan: float
+        ; gate: int }
+
+  type t = Presets of preset PresetMap.t
+
+  let defaultPreset =
+    Preset
+      { start= 0.0
+      ; slicedur= 1.0
+      ; rate= 1.0
+      ; bufnum= 0
+      ; amp= 1.0
+      ; pan= 0.0
+      ; gate= 1 }
+
+  let mk_preset key_id start slicedur rate bufnum amp pan gate =
+    (key_id, Preset {start; slicedur; rate; bufnum; amp; pan; gate})
+
+  let withPitch pitch =
+    let pf = float_of_int pitch in
+    defaultPreset |> fun (Preset pr) -> Preset {pr with rate= Cisp.mtor pf}
+
+  let init =
+    List.init 128 (fun x -> x)
+    |> List.fold_left
+         (fun map k -> PresetMap.add k (withPitch k) map)
+         PresetMap.empty
+    |> fun m -> Presets m
+
+  let lookup key (Presets ps) = PresetMap.find_opt key ps
+end
+
+module PlayingNotes = Map.Make (Int)
+
+type model =
+  | PlaybackManager of
+      { nodeIdManager: UniqueId.t
+      ; playingNotes: UniqueId.id PlayingNotes.t
+      ; presetMem: PresetMemory.t }
+
+let playFrom addr input =
+  let sendSamplerMessage addr (PresetMemory.Preset p) (UniqueId.Id nodeId) =
+    Lo.send addr "/s_new"
+      [ `String "simpleSampler"
+      ; `Int32 nodeId
+      ; `Int32 0
+      ; `Int32 1
+      ; `String "start"
+      ; `Float p.start
+      ; `String "slicedur"
+      ; `Float p.slicedur
+      ; `String "rate"
+      ; `Float p.rate
+      ; `String "bufNum"
+      ; `Int32 p.bufnum
+      ; `String "amp"
+      ; `Float p.amp
+      ; `String "pan"
+      ; `Float p.pan
+      ; `String "gate"
+      ; `Int32 1 ]
+  in
+  let stopNote addr (UniqueId.Id nodeId) =
+    Lo.send addr "/n_set" [`Int32 nodeId; `String "gate"; `Int32 0]
+  in
+  let playNote note (PlaybackManager model) =
+    let presetOpt = PresetMemory.lookup note model.presetMem in
+    match presetOpt with
+    | None -> PlaybackManager model
+    | Some preset -> (
+        let idOpt = UniqueId.get model.nodeIdManager in
+        match idOpt with
+        | id, idState ->
+            sendSamplerMessage addr preset id ;
+            PlaybackManager
+              { model with
+                playingNotes= PlayingNotes.add note id model.playingNotes
+              ; nodeIdManager= idState } )
+  in
+  let stopNote note (PlaybackManager model) =
+    let notesMinusStopped = PlayingNotes.remove note model.playingNotes in
+    let opt = PlayingNotes.find_opt note model.playingNotes in
+    match opt with
+    | Some id ->
+        stopNote addr id ;
+        PlaybackManager {model with playingNotes= notesMinusStopped}
+    | None -> () ; PlaybackManager model
+  in
+  let update msg model =
+    match msg with
+    | Midi.NoteOn (_, Pitch p, _) -> playNote p model
+    | Midi.NoteOff (_, Pitch p, _) -> stopNote p model
+    | _ -> model
+  in
+  let init =
+    PlaybackManager
+      { playingNotes= PlayingNotes.empty
+      ; nodeIdManager= UniqueId.init
+      ; presetMem= PresetMemory.init }
+  in
+  let eval _ = () in
+  input
+  |> fun inp ->
+  Cisp.recursive inp init update eval |> Seq.map (fun () -> Midi.SilenceEvent)
+
+let simpleSamplerPreset =
+  return PresetMemory.mk_preset
+  <*> int_par "voiceNr" <*> flt_par "start" <*> flt_par "slice"
+  <*> flt_par "rate" <*> int_par "bufnum" <*> flt_par "amp" <*> flt_par "pan"
+  <*> int_par "int"
+
+let midiFun addr input =
+  let map = Seq.map in
+  let open Midi in
+  input |> playFrom addr |> Midi.serialize |> map toRaw
+
+let () =
+  let addr = Lo.Address.create "127.0.0.1" 57110 in
+  let f addr =
+    Midi.playMidi (midiFun addr) Process.sample_rate ;
+    while true do
+      Unix.sleep 60
+    done
+  in
+  let (_ : Thread.t) = Thread.create f addr in
+  let _ =
+    Sys.command "jack_disconnect system_midi:capture_2 ocaml_midi:ocaml_midi_in"
+  in
+  let _ =
+    Sys.command
+      "jack_disconnect ocaml_midi:ocaml_midi_out system_midi:playback_1"
+  in
+  let _ =
+    Sys.command "jack_connect ocaml_midi:ocaml_midi_out system_midi:playback_1"
+  in
+  let _ =
+    Sys.command "jack_connect system_midi:capture_2 ocaml_midi:ocaml_midi_in"
+  in
+  while true do
+    Unix.sleep 60
   done
--- examples/sendOsc.ml
+++ examples/sendOsc.ml
@@ -1,4 +1,3 @@
-
 (*
 let bangMessage addr =
   Lo.send addr "/test/" [`Float 3.141592;`String "fish";`Float 2.233;`String "cat";`Float 3.33]
@@ -47,198 +46,175 @@ let testMessage addr =
     ]
  *)
 module UniqueId = struct
-  type t =
-    Pool of   { free : int list
-              ; used : int list }
+  type t = Pool of {free: int list; used: int list}
 
-  type id =
-    Id of int
+  type id = Id of int
 
   let maxId = 65536
-  
-  let init =
-    Pool { free = List.init maxId (fun x -> x + 1)
-    ; used = [] }
+
+  let init = Pool {free= List.init maxId (fun x -> x + 1); used= []}
 
   let free (Pool pool) id =
-    Pool
-      { used = List.filter (fun x -> x = id) pool.used
-      ; free = id :: pool.free }
+    Pool {used= List.filter (fun x -> x = id) pool.used; free= id :: pool.free}
 
   let get (Pool pool) =
     match pool.free with
     | id :: ids ->
-       let newPool =
-         Pool { free = ids
-              ; used = id :: pool.used }
-       in
-       (Id id,newPool)
-    | [] -> (Id 1,Pool
-         { free = List.init maxId (fun x -> x + 2)
-         ; used = [1] })
-    
-  let isFree (Pool pool) (Id id) =
-    List.mem id pool.free  
+        let newPool = Pool {free= ids; used= id :: pool.used} in
+        (Id id, newPool)
+    | [] -> (Id 1, Pool {free= List.init maxId (fun x -> x + 2); used= [1]})
+
+  let isFree (Pool pool) (Id id) = List.mem id pool.free
 end
 
-module PresetMap = Map.Make(Int)
+module PresetMap = Map.Make (Int)
 
 module PresetMemory = struct
   type preset =
-    Preset of {start : float
-               ;slicedur : float
-               ;rate : float
-               ;bufnum : int
-               ;amp : float
-               ;pan : float
-               ;gate: int
-              }
+    | Preset of
+        { start: float
+        ; slicedur: float
+        ; rate: float
+        ; bufnum: int
+        ; amp: float
+        ; pan: float
+        ; gate: int }
 
-  type t =
-    Presets of preset PresetMap.t
+  type t = Presets of preset PresetMap.t
 
-   let defaultPreset =
-    Preset { 
-     start = 0.0
-    ; slicedur = 1.0
-    ; rate = 1.0
-    ; bufnum = 0
-    ; amp = 1.0
-    ; pan = 0.0
-    ; gate = 1
-      }
+  let defaultPreset =
+    Preset
+      { start= 0.0
+      ; slicedur= 1.0
+      ; rate= 1.0
+      ; bufnum= 0
+      ; amp= 1.0
+      ; pan= 0.0
+      ; gate= 1 }
 
-   let withPitch pitch =
-     let pf = float_of_int pitch in
-     defaultPreset
-     |> fun (Preset pr) -> Preset { pr with rate = Cisp.mtor pf }
-  
+  let mk_preset start slicedur rate bufnum amp pan gate =
+    Preset {start; slicedur; rate; bufnum; amp; pan; gate}
+
+  let withPitch pitch =
+    let pf = float_of_int pitch in
+    defaultPreset |> fun (Preset pr) -> Preset {pr with rate= Cisp.mtor pf}
+
   let init =
     List.init 128 (fun x -> x)
     |> List.fold_left
-         (fun map k ->
-           PresetMap.add k (withPitch k) map) PresetMap.empty |> fun m -> Presets m
+         (fun map k -> PresetMap.add k (withPitch k) map)
+         PresetMap.empty
+    |> fun m -> Presets m
 
-  let lookup key (Presets ps) =
-    PresetMap.find_opt key ps
+  let lookup key (Presets ps) = PresetMap.find_opt key ps
+end
 
-end    
+module PlayingNotes = Map.Make (Int)
 
+type model =
+  | PlaybackManager of
+      { nodeIdManager: UniqueId.t
+      ; playingNotes: UniqueId.id PlayingNotes.t
+      ; presetMem: PresetMemory.t }
 
-
-module PlayingNotes = Map.Make(Int)
-
-type model = PlaybackManager of {
-    nodeIdManager : UniqueId.t
-   ;playingNotes : UniqueId.id PlayingNotes.t
-   ;presetMem : PresetMemory.t
-    }
-
-
-      
 let playFrom addr input =
   let sendSamplerMessage addr (PresetMemory.Preset p) (UniqueId.Id nodeId) =
-    Lo.send addr "/s_new" [`String "simpleSampler"
-                    ;`Int32 nodeId
-                    ;`Int32 0
-                    ;`Int32 1
-                    ;`String "start"
-                    ;`Float p.start
-                    ;`String "slicedur"
-                    ;`Float p.slicedur
-                    ;`String "rate"
-                    ;`Float p.rate
-                    ;`String "bufNum"
-                    ;`Int32 p.bufnum
-                    ;`String "amp"
-                    ;`Float p.amp
-                    ;`String "pan"
-                    ;`Float p.pan
-                    ;`String "gate"
-                    ;`Int32 1]
+    Lo.send addr "/s_new"
+      [ `String "simpleSampler"
+      ; `Int32 nodeId
+      ; `Int32 0
+      ; `Int32 1
+      ; `String "start"
+      ; `Float p.start
+      ; `String "slicedur"
+      ; `Float p.slicedur
+      ; `String "rate"
+      ; `Float p.rate
+      ; `String "bufNum"
+      ; `Int32 p.bufnum
+      ; `String "amp"
+      ; `Float p.amp
+      ; `String "pan"
+      ; `Float p.pan
+      ; `String "gate"
+      ; `Int32 1 ]
   in
   let stopNote addr (UniqueId.Id nodeId) =
-    Lo.send addr "/n_set" [`Int32 nodeId
-                         ; `String "gate"
-                         ; `Int32 0]
+    Lo.send addr "/n_set" [`Int32 nodeId; `String "gate"; `Int32 0]
   in
   let playNote note (PlaybackManager model) =
-    let presetOpt =
-      PresetMemory.lookup note model.presetMem
-    in
+    let presetOpt = PresetMemory.lookup note model.presetMem in
     match presetOpt with
-    | None -> (PlaybackManager model)
-    | Some preset ->
-       let idOpt =
-         UniqueId.get model.nodeIdManager
-       in
-       match idOpt with
-        (id,idState) ->
-          sendSamplerMessage addr preset id;
-          PlaybackManager
-            { model with
-              playingNotes = PlayingNotes.add note id model.playingNotes
-            ; nodeIdManager = idState }
+    | None -> PlaybackManager model
+    | Some preset -> (
+        let idOpt = UniqueId.get model.nodeIdManager in
+        match idOpt with
+        | id, idState ->
+            sendSamplerMessage addr preset id ;
+            PlaybackManager
+              { model with
+                playingNotes= PlayingNotes.add note id model.playingNotes
+              ; nodeIdManager= idState } )
   in
   let stopNote note (PlaybackManager model) =
     let notesMinusStopped = PlayingNotes.remove note model.playingNotes in
     let opt = PlayingNotes.find_opt note model.playingNotes in
     match opt with
-    | Some id -> stopNote addr id; PlaybackManager { model with playingNotes = notesMinusStopped }
-    | None -> () 
-            ; PlaybackManager model
+    | Some id ->
+        stopNote addr id ;
+        PlaybackManager {model with playingNotes= notesMinusStopped}
+    | None -> () ; PlaybackManager model
   in
   let update msg model =
     match msg with
-    | Midi.NoteOn (_,(Pitch p),_) -> playNote p model
-    | Midi.NoteOff (_,(Pitch p),_) -> stopNote p model
+    | Midi.NoteOn (_, Pitch p, _) -> playNote p model
+    | Midi.NoteOff (_, Pitch p, _) -> stopNote p model
     | _ -> model
   in
-  let init = PlaybackManager {
-                 playingNotes = PlayingNotes.empty
-                ;nodeIdManager = UniqueId.init
-                ;presetMem = PresetMemory.init
-               }
+  let init =
+    PlaybackManager
+      { playingNotes= PlayingNotes.empty
+      ; nodeIdManager= UniqueId.init
+      ; presetMem= PresetMemory.init }
   in
-  let eval _ =
-    ()
-  in
+  let eval _ = () in
   input
-  |> fun inp -> Cisp.recursive inp init update eval
-                |> Seq.map (fun () -> Midi.SilenceEvent)
-
+  |> fun inp ->
+  Cisp.recursive inp init update eval |> Seq.map (fun () -> Midi.SilenceEvent)
 
 let midiFun addr input =
   let map = Seq.map in
   let open Midi in
-  input
-  |> (playFrom addr)
-  |> Midi.serialize
-  |> map toRaw
-  
+  input |> playFrom addr |> Midi.serialize |> map toRaw
 
 let () =
   (* let open Osc_unix.Udp in
-  let localhost = Unix.inet_addr_of_string "127.0.0.1" in
-  let port = 5665 in
-  let addr = Unix.ADDR_INET (localhost, port) in
-  let client = Client.create () in
-   *)
-   let addr = Lo.Address.create "127.0.0.1" 57110 in
-    
-   let f addr = 
-    Midi.playMidi (midiFun addr) Process.sample_rate 
-    ;while true
-     do Unix.sleep 60 done
-   in
-  let (_:Thread.t) = Thread.create f addr in
-  
-  let _ = Sys.command "jack_disconnect system_midi:capture_2 ocaml_midi:ocaml_midi_in" in
-  let _ = Sys.command "jack_disconnect ocaml_midi:ocaml_midi_out system_midi:playback_1" in
-  let _ = Sys.command "jack_connect ocaml_midi:ocaml_midi_out system_midi:playback_1" in
-  let _ = Sys.command "jack_connect system_midi:capture_2 ocaml_midi:ocaml_midi_in" in
-  while true
-  do
-    Unix.sleep 60 
+     let localhost = Unix.inet_addr_of_string "127.0.0.1" in
+     let port = 5665 in
+     let addr = Unix.ADDR_INET (localhost, port) in
+     let client = Client.create () in
+  *)
+  let addr = Lo.Address.create "127.0.0.1" 57110 in
+  let f addr =
+    Midi.playMidi (midiFun addr) Process.sample_rate ;
+    while true do
+      Unix.sleep 60
+    done
+  in
+  let (_ : Thread.t) = Thread.create f addr in
+  let _ =
+    Sys.command "jack_disconnect system_midi:capture_2 ocaml_midi:ocaml_midi_in"
+  in
+  let _ =
+    Sys.command
+      "jack_disconnect ocaml_midi:ocaml_midi_out system_midi:playback_1"
+  in
+  let _ =
+    Sys.command "jack_connect ocaml_midi:ocaml_midi_out system_midi:playback_1"
+  in
+  let _ =
+    Sys.command "jack_connect system_midi:capture_2 ocaml_midi:ocaml_midi_in"
+  in
+  while true do
+    Unix.sleep 60
   done
- 

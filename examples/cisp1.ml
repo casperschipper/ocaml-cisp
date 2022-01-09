d476426e1cd7bef43fee108dcc001aad22aca893
Casper Schipper <casper.schipper@gmail.com>
2022-01-09 17:19:08 +0100

    before splitting steno into a parser lib

--- examples/cisp1.ml
+++ examples/cisp1.ml
@@ -3,70 +3,39 @@ open Midi
 
 let map = Seq.map
 
-let ofTrigger lst trigger =
-  let notes =
-    seq lst |> floatify |> ( +.- ) 48.0
-    |> map (fun p -> p |> Int.of_float |> mkPitchClip)
-    |> map (fun p -> c3 |> withPitch p |> withDur (seconds 0.1))
+type midiNoteGenerator =
+  MidiNoteGen of { pitch : int Seq.t
+           ; velo : int Seq.t
+           ; durInSec : float Seq.t
+           ; channel : int Seq.t }
+
+let generator =
+  MidiNoteGen { pitch = seq [10;22;23]
+  ; velo = st 100 
+  ; durInSec = seq [0.1;0.2]
+  ; channel = st 1 }
+  
+let midiFun (MidiNoteGen {pitch;velo;durInSec;channel}) input =
+  let durInSamp = map (function s -> s |> ( *. ) !Process.sample_rate |> int_of_float) durInSec in
+  let stream =
+    Seq.return makeNoteOfInts <*> pitch <*> velo <*> durInSamp <*> channel
   in
-  Cisp.weave trigger notes (SilenceEvent |> st)
-
-let thread lst z d trig =
-  let zeros = st 0 |> take z in
-  let ds = st 1 |> take d in
-  let n = [zeros; ds] |> List.to_seq |> transpose |> cycle |> concat in
-  trig |> pulseDivider n |> ofTrigger lst
-
-let toTrigger = map Midi.isNoteOn
-
-let mergeMidiStreams sqa sqb =
-  (* this merges streams of midi events, so it becomes one stream of events. When two events "collide", it pushes one onto the stack.
-     events on the stack have priority on newer events
-  *)
-  let module Q = Fqueue in
-  let handleEvents (a, b) (overflow, _) =
-    let optFlow = Q.peekdeq overflow in
-    match (a, b, optFlow) with
-    | SilenceEvent, SilenceEvent, None -> (Q.empty, SilenceEvent)
-    | SilenceEvent, SilenceEvent, Some (evt, rest) -> (rest, evt)
-    | SilenceEvent, evtb, Some (evt, rest) -> (Q.enqueue evtb rest, evt)
-    | evta, SilenceEvent, Some (evt, rest) -> (Q.enqueue evta rest, evt)
-    | evta, SilenceEvent, None -> (Q.empty, evta)
-    | SilenceEvent, evtb, None -> (Q.empty, evtb)
-    | evta, evtb, None -> (Q.enqueue evtb Q.empty, evta)
-    | evta, evtb, Some (evt, rest) ->
-        (rest |> Q.enqueue evta |> Q.enqueue evtb, evt)
-  in
-  let zipped = Cisp.zip sqa sqb in
-  recursive zipped (Q.empty, SilenceEvent) handleEvents (fun (_, evt) -> evt)
-
-let midiFun input =
-  let sqa = input |> toTrigger |> thread [0; 5; 10; 15; 0; 7; 14; 21] 3 1 in
-  let sqb = input |> toTrigger |> thread [0; 5; 10; 15; 0; 7; 14; 21] 3 2 in
-  let sqc = input |> toTrigger |> thread [0; 12; -12; 0; 7; -7; -14] 3 3 in
-  mergeMidiStreams sqa sqb |> mergeMidiStreams sqc |> serialize |> map toRaw
+    trigger stream input |> serialize |> map toRaw
 
 let () =
   let f () =
-    Midi.playMidi midiFun Process.sample_rate ;
+    Midi.playMidi (midiFun generator) Process.sample_rate ;
     while true do
       Unix.sleep 60
     done
   in
   let _ = Thread.create f () in
-  (*let _ =
-    Sys.command "jack_disconnect system_midi:capture_2 ocaml_midi:ocaml_midi_in"
-  in
-  let _ =
-    Sys.command
-      "jack_disconnect ocaml_midi:ocaml_midi_out system_midi:playback_1"
-  in
   let _ =
     Sys.command "jack_connect ocaml_midi:ocaml_midi_out system_midi:playback_1"
   in
   let _ =
     Sys.command "jack_connect system_midi:capture_2 ocaml_midi:ocaml_midi_in"
-  in *)
+  in 
   while true do
     Unix.sleep 60
   done

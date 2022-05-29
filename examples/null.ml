f6d83619aaa4ff0d39b1c9027338300c81a26add
Casper Schipper <casper.schipper@gmail.com>
2022-05-29 17:23:07 +0200

    Null Recording

--- examples/null.ml
+++ examples/null.ml
@@ -25,7 +25,7 @@ let mkBoermanFading3 () =
   let open Cisp in
   let memsize = 10.0 in
   let tabIndex = seq [0.0;sec memsize]  in
-  let dura = ch [| 0.01; 0.125; 0.25; 0.5; 2.0/.3.0; 3.0/.2.0; 2.0;4.0 |] |> Seq.map (fun x -> x *. memsize)  in
+  let dura = ch [| 0.01;10.0; 1.001;0.999;1.00001;0.9998 |] |> Seq.map (fun x -> x *. memsize)  in
   let readpos = tline dura tabIndex |> Seq.map (clip 0.0 (sec memsize)) in
   let buffer = Array.make (seci memsize) 0.0 in
   let input = Process.inputSeq 0 |> bhpf_static 50.0 0.9  in
@@ -35,7 +35,7 @@ let mkBoermanFading3 () =
     indexCub buffer readpos 
   in
   let joined = effect writer myReader in
-  3.0 *.- joined 
+  2.0 *.- joined 
  
 let all_channels =
   Cisp.rangei 0 15 |> List.of_seq |> List.map (fun _ -> mkBoermanFading3 ())
@@ -55,7 +55,7 @@ let () =
   let _ =
     List.map
       (fun str -> Sys.command str |> ignore)
-      [ "jack_connect system:capture_1 ocaml:input_0"; "jack_lsp -c" ]
+      [ "jack_connect system:capture_5 ocaml:input_0"; "jack_lsp -c" ]
   in
   while true do
     Unix.sleep 60

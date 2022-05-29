e1404a70484bcf3f88a0e02a30022fa13ce8b302
Casper Schipper <casper.schipper@gmail.com>
2022-05-29 17:23:07 +0200

    Null Recording

--- examples/tablepathsplit.ml
+++ examples/tablepathsplit.ml
@@ -1,10 +1,13 @@
-let max = 1024
+let max = 2048
 
-let n_channels = 32
+let n_channels = 16
+
+let () =
+  Random.init 5
 
 let noise = 
-  let open Cisp in
-  lift rvf (-1.0) 1.0 |> Seq.map (fun x -> Float.pow x 10.0) |> take max |> Array.of_seq
+  let open Cisp in 
+  rangei 0 max |> Seq.map (fun x -> x |> float_of_int |> linlin 0.0 (max |> float_of_int) (-1.0) 1.0) |> Array.of_seq
 
 let steps =
   let one_array () = 
@@ -35,11 +38,12 @@ let play_index_table arr =
 let write_split () =
   let idx = Toolkit.rvi 1 (max - 1) in
   let a = Toolkit.rvi 1 (max - 1) in
-  let b = Toolkit.rvi 1 (max - 1) in
-  let c = Toolkit.rvi 1 (max -1) in
+  let b = Toolkit.rvi 1 (idx+1 mod max) in
   let stream = 
     let open Cisp in
-    seq [a;b;c] |> hold (ch [|2;3;4;5;10;100;200;300;400;500|] |> hold (ch [|2;3;5;10;22|])) |> Infseq.cycleSq
+    (* [|lift rv 0 (max -1) |> hold ((seq [17;3;5;6;22;33;1;3000;50000] |> hold (ch [|17;3;5;6;22;33;1;3000;50000|])));ch [|a;b|]|> hold (rv (st 1) (seq [11;2;3;4;400;9000;33000;1;2] |> hold (sometimes 1 2 2)))|] |> fun sqarray -> index_seq sqarray (sometimes 0 1 9000) |> Infseq.cycleSq *)
+  [|lift rv 0 (max -1) |> hold ((seq [17;3;5;6;22;33;1;3000;50000] |> hold (ch [|17;3;5;6;22;33;1;3000;50000|])));ch [|a;b|]|> hold (rv (st 1) (seq [11;2;3;4;400;9000;33000;1;2] |> hold (sometimes 1 2 2)))|] |> fun sqarray -> index_seq sqarray (sometimes 1 0 100 |> hold (ch [|2;3;40;4000;5000|])) |> Infseq.cycleSq
+
     (* Infseq.ch_seq [|(Cisp.ch [|idx;idx;idx;idx;a|] |> Infseq.cycleSq);Infseq.repeat idx|]  *)
   in
   Array.iter (fun array -> array.(idx) <- stream) steps
@@ -49,21 +53,24 @@ let write_normal idx =
   Array.iter (fun array -> array.(idx) <- next) steps
 
 
+let env () =
+  let open Cisp in
+  tline_start 0.0 (fractRandTimer (ch [|1.5;2.7;3.3;5.5;10.3;20.0|])) (seq [0.0;1.0;1.0;0.0])
 
 let () = 
   let open Cisp in
 
-  let chaos = timed (fractRandTimer (ch [|0.001;0.1;1.0;2.0|])) (st write_split |> Seq.map (fun x -> x ()) ) in 
-  let peace = timed (fractRandTimer (ch [|0.001;0.1;0.1;1.0;2.0|])) (countTill max |> Seq.map write_normal) in
+  let chaos = timed (fractRandTimer (ch [|0.05;0.5;0.14;3.0;1.0;9.0|])) (st write_split |> Seq.map (fun x -> x ()) ) in 
+  let peace = timed (fractRandTimer (ch [|2.0;3.0;1.0;1.0|])) (countTill (max-1) |> Seq.map write_normal) in
   let eff = effect_lst masterClock [chaos;peace] in 
-  let signal channel = play_index_table steps.(channel) |> Infseq.index noise |> Infseq.to_seq in
+  let signal channel = play_index_table steps.(channel) |> Infseq.index noise |> Infseq.to_seq |> (fun sign -> sign *.~ env ()) in
   let channels = rangei 1 (n_channels-1)  |> Seq.map (fun n -> signal n) |> List.of_seq in
   
   let with_effect = ((effect eff (signal 0)) :: channels) in
    if true then
-    let size = !Process.sample_rate *. 100.0 |> int_of_float in
+    let size = !Process.sample_rate *. 120.0 |> int_of_float in
     let t = Sndfile.from_seq size (int_of_float !Process.sample_rate) with_effect in
-    Sndfile.write t "/Users/casperschipper/Music/Null/tablepath_pulse_split_2.wav" Sndfile.WAV_32
+    Sndfile.write t "/Users/casperschipper/Music/Null/sawtooth_with_trouble.wav" Sndfile.WAV_32
   else 
     Jack.playSeqs 0 Process.sample_rate with_effect
   

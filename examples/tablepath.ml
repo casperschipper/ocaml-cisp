a5d88cac67d5cc43457a91944935390317bcd2fa
Casper Schipper <casper.schipper@gmail.com>
2023-02-05 19:01:29 +0100

    Good stuff, mouse sounds

--- examples/tablepath.ml
+++ examples/tablepath.ml
@@ -1,20 +1,18 @@
-let max = !Process.sample_rate *. 3.5 |> Cisp.ofFloat
-
+let max = !Process.sample_rate *. 0.01 |> Cisp.ofFloat
 let tone size freq = Cisp.osc freq |> Cisp.take size
 
 let noise =
-  let fromPitch p =
-    p |> ( +. ) 48. |> Cisp.mtof
-  in
-  let ps = 
-    Cisp.walk 0.0 (Cisp.rvf (Cisp.st 6.9) (Cisp.st 7.1)) |> Seq.take 12 |> List.of_seq
+  let fromPitch p = p |> ( +. ) 100. |> Cisp.mtof in
+  let ps =
+    Cisp.walk 0.0 (Cisp.rvf (Cisp.ch [|-(7.1);6.9|]) (Cisp.st 7.1))
+    |> Seq.take 12 |> List.of_seq
   in
   let n = List.length ps in
   let slice = max / n in
-  let freqs =
-    ps |> List.map fromPitch |> List.to_seq
-  in
-  freqs |> Seq.concat_map (fun f -> f |> Seq.repeat |> Seq.take slice |> Cisp.osc) |> Array.of_seq
+  let freqs = ps |> List.map fromPitch |> List.to_seq in
+  freqs
+  |> Seq.concat_map (fun f -> f |> Seq.repeat |> Seq.take slice |> Cisp.osc)
+  |> Array.of_seq
 
 let steps =
   Array.init max (fun idx ->
@@ -22,9 +20,11 @@ let steps =
       Infseq.repeat next)
 
 let get_table arr idx =
-  if idx > (Array.length arr -1) || idx < 0 then None else Some arr.(idx)
+  if idx > Array.length arr - 1 then None
+  else if idx < 0 then None
+  else Some arr.(idx)
 
-let play_index_table arr =
+let play_index_table_first arr first =
   let rec aux arr idx () =
     match get_table arr idx with
     | Some stream -> (
@@ -32,49 +32,49 @@ let play_index_table arr =
         | Infseq.InfCons (nextIdx, tail) ->
             Array.set arr idx tail;
             Infseq.InfCons (idx, aux arr nextIdx))
-    | None -> Infseq.InfCons (0, aux arr 0)
+    | None -> Infseq.InfCons (Toolkit.rvi 0 max, aux arr 0)
   in
-  aux arr 0
+  aux arr first
 
-let write_split () =
-  let idx = Toolkit.rvi 1 (max - 1) in
-  let a = Toolkit.rvi 1 (max - 1) in
-  let b = Toolkit.rvi 1 (max - 1) in
-  let o = Toolkit.rvi 10 11 in
+let play_index_table arr = play_index_table_first arr 0
+
+let write_line () =
+  let open Cisp in
+  let idx = Toolkit.rvi 0 (max - 1) in
   let stream =
-    if Toolkit.rvi 0 30 > 28 then
-      Cisp.ch [| a; b |] |> Cisp.hold (Cisp.st o) |> Infseq.cycleSq
-    else Cisp.seq [ a; b ] |> Cisp.hold (Cisp.st o) |> Infseq.cycleSq
+    tline (ch [|0.001;0.1;0.2;0.3;0.9|])
+      (seq [0.0;Toolkit.rvfi 0.0 (max - 1 |> float_of_int)])
+    |> Infseq.cycleSq |> Infseq.map int_of_float
+    |> Infseq.map (fun x -> Cisp.clip 0 max x)
   in
   steps.(idx) <- stream
 
-  let write_line () =
-    let open Cisp in 
-    let idx = Toolkit.rvi 1 (max - 1) in
-    let stream =
-      Cisp.tline (rvf (st 0.0) (st (max |> float))) (Cisp.ch [|0.1;0.03;0.5;0.01;5.0|]) |> Infseq.cycleSq |> Infseq.map int_of_float
-    in
-    steps.(idx) <- stream
-
 let write_normal idx =
-  let next = Infseq.repeat (idx + 1) in
+  (* let next = Infseq.repeat (Toolkit.wrap 0 max (idx + 1)) in *)
+  let next =
+    Cisp.weights [ (idx+1, 10000000); (Toolkit.rvi 0 (max - 1), 1) ]
+    |> Infseq.cycleSq
+  in
   steps.(idx) <- next
 
 let () =
   let open Cisp in
   let chaos =
-    timed
-      (fractRandTimer (ch [| 0.01 |]))
-      (st write_line |> Seq.map (fun x -> x ()))
-  in
+       timed
+         (tline (st 7.0) (seq [0.01;6.0]))
+         (st write_line |> Seq.map (fun x -> x ()))
+     in
   let peace =
     timed
-      (fractRandTimer (ch [|0.01;0.1;0.4; 0.5;4.0|]))
-      (countTill 255 |> Seq.map write_normal)
+      (tline (rvf (st 3.0) (st 8.0)) (seq [0.001;1.0]))
+      ((countTill max) |> Seq.map write_normal) 
   in
-  let eff = effect_lst masterClock [ chaos; peace ] in
+  let eff = effect_lst masterClock [ chaos;peace ] in
   let signal () =
-    play_index_table steps |> Infseq.index noise |> Infseq.to_seq
+    let start = Toolkit.rvi 0 max in
+    play_index_table_first steps start |> Infseq.index noise |> Infseq.to_seq
   in
-  let channels = rangei 0 1 |> Seq.map (fun _ -> signal ()) |> List.of_seq in
-  Jack.playSeqs 0 Process.sample_rate (effect eff (signal ()) :: channels)
+  let channels =
+    rangei 0 1 |> Seq.map (fun _ -> mkLots 5 signal) |> List.of_seq
+  in
+  Jack.playSeqs 0 Process.sample_rate (effect eff (mkLots 4 signal) :: channels)

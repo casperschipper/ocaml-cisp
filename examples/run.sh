95f0c175869a7bcdf1d5cb16cc7cb969d3edfd39
Casper Schipper <casper.schipper@gmail.com>
2025-01-12 10:04:20 +0100

    added run.sh and ws stub

---
+++ examples/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ 
+dune exec ./simplecasper.exe
---
+++ lib/dream_websocket/dream_websocket.ml
@@ -0,0 +1,41 @@
+let home =
+{|
+  <html>
+  <body>
+    <script>
+
+    var socket = new WebSocket(\"ws://\" + window.location.host + \"/websocket\");
+
+    socket.onopen = function () {
+      socket.send(\"Hello?\");
+    };
+
+    socket.onmessage = function (e) {
+      alert(e.data);
+    };
+
+    </script>
+  </body>
+  </html>
+  |}
+
+
+let () =
+  Dream.run
+  @@ Dream.logger
+  @@ Dream.router [
+
+    Dream.get "/"
+      (fun _ ->
+        Dream.html home);
+
+    Dream.get "/websocket"
+      (fun _ ->
+        Dream.websocket (fun websocket ->
+          match%lwt Dream.receive websocket with
+          | Some "Hello?" ->
+            Dream.send websocket "Good-bye!"
+          | _ ->
+            Dream.close_websocket websocket));
+
+  ]

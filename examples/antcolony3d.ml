ee12fbafb49ce84b211ea3f3b8bee012fb2907d2
Casper Schipper <casper.schipper@gmail.com>
2026-01-17 16:42:08 +0100

    this works rather ok

--- examples/antcolony3d.ml
+++ examples/antcolony3d.ml
@@ -1827,7 +1827,7 @@ let send_osc sender time evt =
 let supercollider_sched nodes_stream =
   let scheduler_samps = 4048 in
   let scheduler_sec = Cisp.seconds_from_samples scheduler_samps in
-  let event_of_node node = (0.1, test_jv_event (get_node_id node)) in
+  let event_of_node node = (0.03, test_jv_event (get_node_id node |> (fun x -> x mod 49))) in
   let event_sq = nodes_stream |> Infseq.of_seq |> Infseq.map event_of_node in
   let sched =
     Clockscheduler.create ~interval:scheduler_sec ~overlap:1.25 ~seq:event_sq
